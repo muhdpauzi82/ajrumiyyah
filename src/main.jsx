96aa06b46337889ebeb7263c5213ab0d3b6d0f24
muhdpauzi1982 <muhammadpauzi1982@gmail.com>
2026-08-15 22:46:24 +0800

    Ajrumiyyah PWA

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,33 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+import { registerSW } from "virtual:pwa-register";
+
+import "./index.css";
 import "./ui/ads.css";
 import "./ui/theme/index.css";
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+import App from "./App.jsx";
+
+
+/* =========================================================
+   PWA SERVICE WORKER
+   Auto update apabila versi baharu telah dideploy
+========================================================= */
+
+registerSW({
+  immediate: true,
+});
+
+
+/* =========================================================
+   REACT
+========================================================= */
+
+createRoot(
+  document.getElementById("root")
+).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
-
+  </StrictMode>
+);
